9a41c34d7bbb152884eafb94aacfe9244c42c723
ITS - Mauricio Munoz <mauricio.munoz@its.com.co>
2023-01-29 01:23:52 -0500

    cambio de Redis a Postgres

--- app.py
+++ app.py
@@ -1,23 +1,50 @@
+from flask import Flask
+import psycopg2
 import time
 
-import redis
-from flask import Flask
-
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
+retries = 5
+
+while retries != 0:
+    try:
+        db = psycopg2.connect(
+            host="postgres",
+            user="postgres",
+            port='5432',
+            password="myPassword"
+        )
+        retries = 0
+    except:
+        if retries == 0:
+            print("An error has ocurred while connecting to database")
+        else:
+            retries -=1
             time.sleep(0.5)
+
+cursor = db.cursor()
+cursor.execute('DROP TABLE IF EXISTS Counter;')
+cursor.execute('CREATE TABLE Counter(id SERIAL PRIMARY KEY, value INTEGER);')
+cursor.execute('INSERT INTO Counter(value) VALUES (0);')
+db.commit()
+
+cursor.close()
+db.close()
+
+def get_db_connection():
+    conn = psycopg2.connect(
+                            host="postgres",
+                            user="postgres",
+                            password="myPassword",
+                            port='5432')
+    return conn
 
 @app.route('/')
 def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM Counter;')
+    count = cur.fetchone()
+    cur.close()
+    conn.close()
+    return 'Hello World! {}'.format(count[1])
